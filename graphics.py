95f8162e9151a87bc4dde537b7c29f20b61ef293
JedizR <teenday.jedi@gmail.com>
2025-01-17 15:44:14 +0700

    mvp for graphics mocking

--- graphics.py
+++ graphics.py
@@ -9,10 +9,13 @@ pygame.init()
 
 # Colors
 WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
+BLACK = (30, 30, 30)
 RED = (255, 0, 0)
-GREEN = (0, 255, 0)
+GREEN = (0, 200, 50)
 BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+PURPLE = (255, 0, 255)
+AQUA = (0, 255, 255)
 
 # Sceen
 SCREEN_WIDTH = 800
@@ -35,13 +38,18 @@ clocks = pygame.time.Clock()
 running = True  
 
 class MockPlayer():
-    def __init__(self, x, y, direction, color, radius):
+    def __init__(self, name, x, y, direction, color, radius, forward_key, turn_left_key, turn_right_key):
+        self.name = name
         self.x = x
         self.y = y
         self.dir = direction
         self.color = color
         self.radius = radius
         self.speed = 0
+        self.score = 0
+        self.forward_key = forward_key
+        self.turn_left_key = turn_left_key
+        self.turn_right_key = turn_right_key
         
     def update(self):
         self.x += math.cos(self.dir) * self.speed
@@ -61,11 +69,11 @@ class MockPlayer():
         
             
     def accelerate_and_turn(self, Key):
-        if Key[pygame.K_d]:
+        if Key[self.turn_right_key]:
             self.dir -= TURN_SPEED
-        elif Key[pygame.K_a]:
+        elif Key[self.turn_left_key]:
             self.dir += TURN_SPEED
-        if Key[pygame.K_w]:
+        if Key[self.forward_key]:
             self.speed += ACCELERATION
         
         if self.dir >= 2*math.pi:
@@ -78,31 +86,44 @@ class MockPlayer():
         if distance <= food.radius + self.radius:
             return True
         return False
+    
+    def check_player_eatable(self, player):
+        distance = math.sqrt((self.x-player.x)**2 + (self.y-player.y)**2)
+        if distance <= self.radius-player.radius and self.radius-player.radius >= 3:
+            return True
+        return False
         
     def draw(self):
         pygame.draw.circle(screen, self.color, (int(self.x), int(self.y)), self.radius)
-        pygame.draw.line(screen, self.color, (self.x,self.y), 
-                                            (self.x+self.radius*2*math.cos(self.dir),
-                                             self.y-self.radius*2*math.sin(self.dir)), self.radius//2)
+        pygame.draw.circle(screen, WHITE, ((self.x+self.radius*0.5*math.cos(self.dir), self.y-self.radius*0.5*math.sin(self.dir))), self.radius//3)
+        
+
+
 
 class MockFood():
-    def __init__(self, x, y):
+    def __init__(self, x, y, color, radius):
         self.x = x
         self.y = y
-        self.color = BLACK
-        self.radius = 10
+        self.color = color
+        self.radius = radius
         
     def draw(self):
         pygame.draw.circle(screen, self.color, (int(self.x), int(self.y)), self.radius)
 
 def mock_food_generator(food_list):
-    food = MockFood(random.randint(50, SCREEN_WIDTH),random.randint(50, SCREEN_HEIGHT))
+    food = MockFood(random.randint(50, SCREEN_WIDTH),random.randint(50, SCREEN_HEIGHT), random.choice([RED,GREEN,BLUE,YELLOW,PURPLE,AQUA]), 5)
     food_list.append(food)
 
 
-player1 = MockPlayer(100,100,1.5*math.pi, RED, 20)
-player2 = MockPlayer(200,200,1.5*math.pi, GREEN, 20)
-player3 = MockPlayer(300,300,1.5*math.pi, BLUE, 20)
+player1 = MockPlayer("Jedi", random.randint(50, SCREEN_WIDTH),
+                     random.randint(50, SCREEN_HEIGHT),
+                     1.5*math.pi, RED, 8, pygame.K_w, pygame.K_a, pygame.K_d)
+player2 = MockPlayer("Aomsub",random.randint(50, SCREEN_WIDTH),
+                     random.randint(50, SCREEN_HEIGHT),
+                     1.5*math.pi, GREEN, 8, pygame.K_t, pygame.K_f, pygame.K_h)
+player3 = MockPlayer(r"Champ",random.randint(50, SCREEN_WIDTH),
+                     random.randint(50, SCREEN_HEIGHT),
+                     1.5*math.pi, BLUE, 8, pygame.K_i, pygame.K_j, pygame.K_l)
 
 food_list = []
 
@@ -112,35 +133,55 @@ player_list = [
     player3
 ]
 
-score = 0
-
-def show_score(score):
+def update_score_board(player_list):
     font = pygame.font.Font(None, 32)
-    text = font.render(f"Score: {score}", True, GREEN)
-    screen.blit(text, (10, 10))
-
+    header_text = "Scoreboard"
+    header = font.render(header_text, True, WHITE)
+    screen.blit(header, (10, 10))
+    player_list.sort(reverse=True, key=lambda x: x.score)
+    for i,player in enumerate(player_list):
+        score_text = f"{i+1}. {player.name} : {player.score}"
+        score_board = font.render(score_text, True, player.color)
+        screen.blit(score_board, (10, 15+25*(i+1)))
+    
 while running:
-    screen.fill(WHITE)
+    screen.fill(BLACK)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     
+    # ------------ simulate engine -----------
     keys = pygame.key.get_pressed()
     
-    if not food_list:
+    if len(food_list) != 50:
         mock_food_generator(food_list)
+    # ----------------------------------------
     
     for food in food_list:
-        food.draw()
-        if player1.check_collision(food):
-            food_list.remove(food)
-            mock_food_generator(food_list)
-            score += 1
+        food.draw() # grafics function
+        # ------------ simulate engine -----------
+        for player in player_list:
+            if player.check_collision(food):
+                food_list.remove(food)
+                mock_food_generator(food_list)
+                player.score += 1
+                player.radius += 0.5
+        # ----------------------------------------
+    player_list.sort(key=lambda x: x.score)
+    for player in player_list:
+        # ------------ simulate engine -----------
+        for other_player in player_list:
+            if player.check_player_eatable(other_player):
+                player_list.remove(other_player)
             
-    show_score(score)
-    player1.accelerate_and_turn(keys)
-    player1.update()
-    player1.draw()
+        # ------------------------------------
+        player.accelerate_and_turn(keys)
+        player.update()
+        # ----------------------------------------
+        player.draw() # graphics function
+    player_list.sort(reverse=True, key=lambda x: x.score)
+        
+    update_score_board(player_list) # graphics function
     
     pygame.display.flip()
     
